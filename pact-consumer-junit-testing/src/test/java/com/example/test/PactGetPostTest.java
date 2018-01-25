eb08ed97fb466555e77b1f20bc666aec121f801d
Vishwa Vijay <im.vishwavijay@gmail.com>
2018-01-25 08:03:15 +0530

    refactored code implemented get method

---
+++ pact-consumer-junit-testing/src/test/java/com/example/test/PactGetPostTest.java
@@ -0,0 +1,43 @@
+package com.example.test;
+
+import static au.com.dius.pact.consumer.ConsumerPactRunnerKt.runConsumerTest;
+import static org.junit.Assert.assertEquals;
+
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.Assert;
+import org.junit.Test;
+
+import au.com.dius.pact.consumer.ConsumerPactBuilder;
+import au.com.dius.pact.consumer.PactVerificationResult;
+import au.com.dius.pact.model.MockProviderConfig;
+import au.com.dius.pact.model.RequestResponsePact;
+
+public class PactGetPostTest {
+	
+	@Test
+	public void getTest() {
+
+		String path = "/hello";
+		String responseBody = "{\"hello\":\"harry\"}";
+
+		RequestResponsePact pact = ConsumerPactBuilder.consumer("Inventary status consumer")
+				.hasPactWith("Inventary status provider").uponReceiving("Request for available item").path(path)
+				.method(HttpGet.METHOD_NAME).willRespondWith().status(200).body(responseBody).toPact();
+
+		MockProviderConfig config = MockProviderConfig.createDefault();
+
+		PactVerificationResult result = runConsumerTest(pact, config, mockServer -> {
+			String response = Request.Get(mockServer.getUrl() + path)
+					.addHeader("testreqheader", "testreqheadervalue")
+					.execute().returnContent().asString();
+			Assert.assertEquals("Response is invalid.", response, responseBody);
+		});
+
+		if (result instanceof PactVerificationResult.Error) {
+			throw new RuntimeException(((PactVerificationResult.Error) result).getError());
+		}
+
+		assertEquals(PactVerificationResult.Ok.INSTANCE, result);
+	}
+}
